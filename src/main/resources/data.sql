2cc59d2b1ccded0de7bb7899cb9e788d7ecc1fc8
Castanheta <castanheta@ua.pt>
2025-05-21 18:01:34 +0100

    Loading initial POI entries to database automatically

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO points_of_interest (id, name, latitude, longitude, metadata, rating, thumbnail) VALUES
+  (1,'Aveiro Cathedral', 40.6443, -8.6455, 'Aveiro', 5, NULL),
+  (2,'Museu Marítimo de Ílhavo', 40.6017, -8.6680, 'Ílhavo', 5, NULL),
+  (3,'Forum Aveiro', 40.6419, -8.6531, 'Aveiro', 4, NULL),
+  (4,'Moliceiro Boat Tour', 40.6405, -8.6538, 'Aveiro', 5, NULL),
+  (5,'Costa Nova Beach', 40.6111, -8.7497, 'Costa Nova', 5, NULL),
+  (6,'Pateira de Fermentelos', 40.5742, -8.5072, 'Águeda', 4, NULL),
+  (7,'Salinas de Aveiro', 40.6400, -8.6630, 'Aveiro', 4, NULL);
